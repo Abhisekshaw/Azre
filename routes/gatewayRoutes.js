37d898162e5aad5ad36b0c3e3b8f5b86e2a27184
Abhisekshaw <abhishek.shaw@aaensa.com>
2025-06-13 18:29:05 +0530

    removed protection

--- routes/gatewayRoutes.js
+++ routes/gatewayRoutes.js
@@ -7,7 +7,7 @@ const { registerDevice } = require('../controller/AddDevice/addDevice');
 
 
 // GET /gateway-data?start=unixStart&end=unixEnd
-router.post('/gateway-data/:device',protect, getGatewayDataByTimeRange);
-router.get('/gateway-list/:device',protect, gatewayList);
-router.post('/register-device', protect, registerDevice);
+router.post('/gateway-data/:device', getGatewayDataByTimeRange);
+router.get('/gateway-list/:device', gatewayList);
+router.post('/register-device', registerDevice);
 module.exports = router;
